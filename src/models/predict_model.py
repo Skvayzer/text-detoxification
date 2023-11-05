432f3403c5c595d99e05cb1a5190848c8f39b37e
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-11-05 17:18:30 +0300

    made train and predict files for T5

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -3,70 +3,27 @@ import sys
 import pandas as pd
 from tqdm import tqdm
 
-from dotenv import find_dotenv, load_dotenv
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-load_dotenv()
-root = os.path.dirname(find_dotenv())
-sys.path.append(root)
+import transformers
 
 
-def test(
-    model,
-    test_data,
-    tokenizer,
-    batch_size=100,
-):
-    """
-    Test function for T5 model: adds prefix to source and trim sequence.
-    Then feed to the model and decode the output
-    """
+ModelTokenizer = transformers.models.t5.tokenization_t5_fast.T5TokenizerFast
+ModelType = transformers.models.t5.modeling_t5.T5ForConditionalGeneration
 
-    prefix = "make sentence non-toxic:"
-    toxic = "source"
-    res = pd.DataFrame({"source": test_data[toxic]})
-    model_res = []
+def inference(text_model: ModelType, inference_request: str, tokenizer: ModelTokenizer) -> str:
+    input_ids = tokenizer(inference_request, return_tensors="pt").input_ids
+    outputs = text_model.generate(input_ids=input_ids)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True, temperature=0)
 
-    for i in tqdm(range(0, len(test_data), batch_size)):
-        batch = test_data[i : i + batch_size]
-        input_texts = [prefix + line for line in batch[toxic]]
-
-        input_ids = tokenizer(
-            input_texts,
-            return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=128,
-        ).input_ids
-        outputs = model.generate(input_ids=input_ids)
-
-        decoded_outputs = [
-            tokenizer.decode(output, skip_special_tokens=True) for output in outputs
-        ]
-        model_res.extend(decoded_outputs)
-
-    res["target"] = model_res
-    return res
-
-
-def predict(model_name=None, model_base="t5-small", dataset=None):
+def predict(model_cktp_path="t5-small", text="Fuck it I'm done with this shit goddamn"):
     """Function to get a list of predictions from the model"""
 
-    if dataset is None:
-        data_dir = os.getenv("DATA_DIR")
-        dataset = root + "/" + data_dir + "/interim/dataset/test.csv"
-        dataset = pd.read_csv(dataset, index_col=0)
-
-    if model_name is None:
-        model_dir = os.getenv("MODEL_DIR")
-        model_name = root + "/" + os.path.join(model_dir, "t5-small-ft")
-
     # loading the model and run inference for it
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    model = transformers.AutoModelForSeq2SeqLM.from_pretrained(model_cktp_path)
     model.eval()
     model.config.use_cache = False
-    tokenizer = AutoTokenizer.from_pretrained(model_base)
+    tokenizer = AutoTokenizer.from_pretrained(model_cktp_path)
 
-    res = test(model, dataset, tokenizer)
+    res = inference(model, text, tokenizer)
 
     return res
